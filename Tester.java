7395c871eab2e5aebb5521295f9187863db8ab2a
Emily <qtan20@stuy.edu>
2020-10-23 13:30:32 -0400

    I tested this program and it works now

--- Tester.java
+++ Tester.java
@@ -1,7 +1,24 @@
 public class Tester{
+
+  public static String arrToString(int[] arr){
+    String newString = "{";
+    for(int i = 0; i < arr.length - 1; i++){
+      newString += arr[i];
+      newString += ", ";
+    }
+    if(arr.length > 0){
+      newString += (arr[arr.length-1]);
+    }
+    newString += "}";
+    return newString;
+  }
+
   public static void main(String[] args){
     int[] testerArray = {3, 4, 5};
+    int[][] tester2dArray = {{1, 2, 3},{4, 5, 6}, {7, 8, 9}};
+
     System.out.println(ArrayOps.sum(testerArray));
     System.out.println(ArrayOps.largest(testerArray));
+    System.out.println(arrToString(ArrayOps.sumRows(tester2dArray)));
   }
 }
