76bb693c27a21fa8d797fa2b76348e4aefd68b1b
Emily <qtan20@stuy.edu>
2020-10-23 13:20:24 -0400

    I tested this program and it works now

--- Tester.java
+++ Tester.java
@@ -2,5 +2,6 @@ public class Tester{
   public static void main(String[] args){
     int[] testerArray = {3, 4, 5};
     System.out.println(ArrayOps.sum(testerArray));
+    System.out.println(ArrayOps.largest(testerArray));
   }
 }
